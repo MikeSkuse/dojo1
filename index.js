a3f9158eaee8d43f375e98c45704a279137f3b65
P400896 <p400896@MS-MacBook-Pro.local>
2018-12-20 15:58:15 +0000

    blah

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 8001
 const log = require('./lib/log')
 
 var appzip = require('appmetrics-zipkin')({
-    host: '10.0.184.70',
+    host: 'http://10.0.184.70',
     port: 9411,
     serviceName:pkg.name,
     sampleRate: 1.0
